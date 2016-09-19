b1ca73b1543931151750733873dcea4994515c7c
Ricky <ricky@digi.me>
2016-09-19 13:01:28 +0100

    Committing an actual html file

--- helloworld.js
+++ helloworld.js
@@ -6,13 +6,21 @@ var Person = (function () {
         this.firstName = name;
     }
     return Person;
-})();
-
+}());
 function greeter(person) {
-    return "Hello, " + person.firstName;
+    var greeting = "<h1>Hello, " + person.firstName + "</h1>";
+    var timestamp = "This was called on " + getTimestamp();
+    return greeting + "<br>" + timestamp;
 }
-
+function getTimestamp() {
+    return "" + new Date().getTime();
+}
 // define a new Peson instance
 var user = new Person("Jane User");
-
 document.body.innerHTML = greeter(user);
+var button = document.createElement("button");
+button.textContent = "Say something!";
+button.onclick = function () {
+    alert("something!");
+};
+document.body.appendChild(button);
